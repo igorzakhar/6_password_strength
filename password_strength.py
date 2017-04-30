acaee2bb5f121a208d5b3b59f41c3b7c26862a76
igorzakhar <igorzakhar@gmail.com>
2017-04-30 23:23:12 +0300

    Added requirements

--- password_strength.py
+++ password_strength.py
@@ -2,33 +2,93 @@
 
 import re
 
+from terminaltables import AsciiTable
+
 
 def get_password_strength(password):
+    if not password:
+        return
+
+    numbers_score = 0
+    lower_letters_score = 0
+    upper_letters_score = 0
+    symbols_score = 0
     lenght = len(password)
+    requirements = 0
+    requirements_score = 0
 
-    digits = re.findall('\d+', password)
-    lower_letter = re.findall('[a-z]', password)
+#---------/regexp/-----------------
+    #requirements_pattern = r'^.*(?=.{1,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&+=]).*$'
+    #requirements = re.search(requirements_pattern, password)
+    digits = ''.join(re.findall('\d+', password))
     upper_letter = re.findall('[A-Z]', password)
-    symbols = re.findall('.,[,!,@,#,$,%,^,&,*,(,),_,~,-,]', password)
+    print('Uppercase: ', upper_letter)
+    lower_letter = re.findall('[a-z]', password)
+    print('Lowercase: ', lower_letter)
+    symbols = ''.join(re.findall(r'[~!@#$%^&*+=]+', password))
+    print('Symbols: ', symbols)
+    numbers_only = re.search('^\d+$', password)
+    
+#---------/calculation of metric's/-----------------
+    
+    characters_score = lenght * 4       
+    if not numbers_only:
+        numbers_score = len(digits) * 4
+        requirements += 1
+    if lower_letter:
+        lower_letters_score = (lenght - len(lower_letter)) * 2
+        requirements += 1
+    if upper_letter:
+        upper_letters_score = (lenght - len(upper_letter)) * 2
+        requirements += 1
+    if symbols:
+        symbols_score = len(symbols) * 6
+        requirements += 1
+    if lenght >= 8:
+        requirements += 1
+    if requirements == 5:
+        requirements_score = requirements * 2
 
-    num_of_characters = lenght * 4
-    numbers = len(digits)
-    num_lower_letters = len(lower_letter)
+    
+    print('Length: ', lenght, 'symbols')
+    
+    total = (characters_score + upper_letters_score + lower_letters_score + numbers_score + symbols_score + requirements_score)
 
-    print(num_of_characters)
-    print(digits)
-    print(lower_letter, num_lower_letters)
+
+    table_data = [['Metric\'s', 'Rate','Count','Score'],
+                  ['Number of Characters', '+(n*4)', 
+                   lenght, '+' + str(characters_score)],
+                  ['Uppercase Letters', '+((len-n)*2)', 
+                   len(upper_letter), '+' + str(upper_letters_score)],
+                  ['Lowercase Letters', '+((len-n)*2)', 
+                   len(lower_letter), '+' + str(lower_letters_score)],
+                  ['Numbers', '+(n*4)', 
+                   len(digits), '+' + str(numbers_score)],
+                  ['Symbols', '+(n*6)', 
+                   len(symbols), '+' + str(symbols_score)],
+                   ['Requirements', '+(n*2)', 
+                   requirements, '+' + str(requirements_score)], 
+                  ['','', 'Total', total]
+                  ]
+
+    table = AsciiTable(table_data)
+    table.inner_row_border = True
+    table.justify_columns = {0: 'left', 1: 'center', 2: 'center', 3: 'center'}
+    print(table.table)
     
     #return digits, lower_letter, upper_letter, symbols
 
 def get_user_password():
-    return input('Enter password: ')
+    password = input('Enter password: ')
+    if not password:
+        print('Password field cannot be empty')
+    else:
+        return password
 
 
 if __name__ == '__main__':
     password = get_user_password()
-    print(get_password_strength(password))
-
+    get_password_strength(password)
 
 
 """
@@ -37,4 +97,19 @@ if __name__ == '__main__':
         print("ZAPRET")
     else:
         return strength_score
+"""
+
+"""
+print(
+        'Number of Char.(+(n*4)). n = {}. Score = {}'.format(
+            lenght, characters_score))
+    print(
+        'Lower Letters. +((len-n)*2). n = {}. Score = {}'.format(
+            len(lower_letter), lower_letters_score))
+    print(
+        'Upper Letters. +((len-n)*2). n = {}. Score = {}'.format(
+            len(upper_letter), upper_letters_score))
+    print(
+        'Numbers +(n*4). n = {}. Score = {}'.format(
+            len(digits), numbers_score))
 """
