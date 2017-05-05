b3ae124197d7e526e52624f19fb23e2d346f8eaa
igorzakhar <igorzakhar@gmail.com>
2017-05-05 09:38:09 +0300

    Decomposition all functions

--- password_strength.py
+++ password_strength.py
@@ -6,7 +6,7 @@ import getpass
 from terminaltables import AsciiTable
 
 
-def get_user_password(hide):
+def input_user_password(hide):
     if hide:
         password = getpass.getpass(prompt='Enter password: ')
     else:
@@ -14,248 +14,163 @@ def get_user_password(hide):
     return password
 
 
-def check_sequential_alphas(password):
-    sequential_score = 0
+def get_number_characters_score(password):
+    number_characters_count = len(password)
+    number_characters_score = number_characters_count * 4
+    return number_characters_score, number_characters_count
+
+
+def get_uppercase_score_req(password):
+    upper_letters_score = 0
+    upper_letter = re.findall(r'[A-Z]', password)
+    upper_letters_count = len(upper_letter)
+    if upper_letter:
+        upper_letters_score = (len(password) - upper_letters_count) * 2
+    return upper_letters_score, upper_letters_count
+
+
+def get_lowercase_score_req(password):
+    lower_letters_score = 0
+    lower_letter = re.findall(r'[a-z]', password)
+    lower_letter_count = len(lower_letter)
+    if lower_letter:
+        lower_letters_score = (len(password) - lower_letter_count) * 2
+    return lower_letters_score, lower_letter_count
+
+
+def get_numbers_score_req(password):
+    numbers_score = 0
+    digits_list = re.findall(r'\d+', password)
+    digits_count = len(''.join(digits_list))
+    if not password.isdigit() and digits_list:
+        numbers_score = digits_count * 4
+    return numbers_score, digits_count
+
+
+def get_symbol_score_req(password):
+    symbols_score = 0
+    symbols = ''.join(re.findall(r'[^a-zA-Z0-9_]', password))
+    symbols_count = len(symbols)
+    if symbols:
+        symbols_score = symbols_count * 6
+    return symbols_score, symbols_count
+
+
+def get_middle_num_symb_score(password):
+    middle_num_symb_score = 0
+    midle_num_symb = ''.join(re.findall(r'[^a-zA-Z_]+', password[1:-1]))
+    midle_num_symb_count = len(midle_num_symb)
+    if midle_num_symb:
+        middle_num_symb_score = midle_num_symb_count * 2
+    return middle_num_symb_score, midle_num_symb_count
+
+
+def get_letters_only_score(password):
+    letters_only_score = 0
+    letters_only = ''.join(re.findall(r'^[a-zA-Z]+$', password))
+    letters_only_count = len(letters_only)
+    if letters_only:
+        letters_only_score = -letters_only_count
+    return letters_only_score, letters_only_count
+
+
+def get_numbers_only_score_req(password):
+    numbers_only_score = 0
+    numbers_only = ''.join(re.findall(r'^\d+$', password))
+    numbers_only_count = len(numbers_only)
+    if numbers_only:
+        numbers_only_score = -numbers_only_count
+    return numbers_only_score, numbers_only_count
+
+
+def get_consecutive_upper_score(password):
+    consecutive_upper_score = 0
+    consecutive_upper_list = re.findall(r'[A-Z]{2,}', password)
+    consecutive_upper_count = sum(
+        [(len(item)-1) for item in consecutive_upper_list])
+    if consecutive_upper_count:
+        consecutive_upper_score = -(consecutive_upper_count * 2)
+    return consecutive_upper_score, consecutive_upper_count
+
+
+def get_consecutive_lower_score(password):
+    consecutive_lower_score = 0
+    consecutive_lower_list = re.findall(r'[a-z]{2,}', password)
+    consecutive_lower_count = sum(
+        [(len(item)-1) for item in consecutive_lower_list])
+    if consecutive_lower_count:
+        consecutive_lower_score = -(consecutive_lower_count * 2)
+    return consecutive_lower_score, consecutive_lower_count
+
+
+def get_consecutive_number_score(password):
+    consecutive_number_score = 0
+    consecutive_num_list = re.findall(r'[0-9]{2,}', password)
+    consecutive_num_count = sum(
+        [(len(item)-1) for item in consecutive_num_list])
+    if consecutive_num_count:
+        consecutive_number_score = -(consecutive_num_count * 2)
+    return consecutive_number_score, consecutive_num_count
+
+
+def get_sequential_letters_score(password):
+    sequential_letters_count = 0
+    sequential_letters_score = 0
     for item in range(len(string.ascii_lowercase)):
         str_forward = string.ascii_lowercase[item:item + 3]
         str_reverse = str_forward[::-1]
         if (password.lower().find(str_forward) != -1 or
             password.lower().find(str_reverse) != -1):
-            sequential_score += 1
-    return sequential_score
+            sequential_letters_count += 1
+    if sequential_letters_count:
+        sequential_letters_score = -(sequential_letters_count * 3)
+    return sequential_letters_score, sequential_letters_count
 
 
-def check_sequential_numerics(password):
-    sequential_score = 0
+def get_sequential_numbers_score(password):
+    sequential_numbers_count = 0
+    sequential_numbers_score = 0
     for item in range(len(string.digits)):
         str_forward = string.digits[item:item + 3]
         str_reverse = str_forward[::-1]
         if (password.lower().find(str_forward) != -1 or
             password.lower().find(str_reverse) != -1):
-            sequential_score += 1
-    return sequential_score
+            sequential_numbers_count += 1
+    if sequential_numbers_count:
+        sequential_numbers_score = -(sequential_numbers_count * 3)
+    return sequential_numbers_score, sequential_numbers_count
 
 
-def get_additions_type(password):
-    length = len(password)
-    digits_list = re.findall(r'\d+', password)
-    upper_letter = re.findall(r'[A-Z]', password)
-    lower_letter = re.findall(r'[a-z]', password)
-    symbols = ''.join(re.findall(r'[^a-zA-Z0-9_]', password))
-    midle_num_symb = ''.join(re.findall(r'[^a-zA-Z_]+', password[1:-1]))
-    numbers_only = ''.join(re.findall(r'^\d+$', password))
-
-    return (length, digits_list, upper_letter, lower_letter,
-            symbols, midle_num_symb, numbers_only)
-
-
-def get_deductions_type(password):
-    numbers_only = ''.join(re.findall(r'^\d+$', password))
-    letters_only = ''.join(re.findall(r'^[a-zA-Z]+$', password))
-
-    consecutive_upper_list = re.findall(r'[A-Z]{2,}', password)
-    consecutive_lower_list = re.findall(r'[a-z]{2,}', password)
-    consecutive_num_list = re.findall(r'[0-9]{2,}', password)
-    consecutive_upper_count = sum(
-        [(len(item)-1) for item in consecutive_upper_list])
-    consecutive_lower_count = sum(
-        [(len(item)-1) for item in consecutive_lower_list])
-    consecutive_num_count = sum(
-        [(len(item)-1) for item in consecutive_num_list])
-
-    sequense_letters = re.findall(r'[A-Za-z]{3,}', password)
-    sequense_numbers = re.findall(r'[0-9]{3,}', password)
-
-    return (numbers_only, letters_only, 
-            consecutive_upper_count, consecutive_lower_count, consecutive_num_count, sequense_letters, sequense_numbers)
-
-
-def get_additions_score(additions_type):
-    (lenght, digits_list, 
-     upper_letter, lower_letter,
-     symbols, midle_num_symb, numbers_only) = additions_type
-    
-    numbers_score = 0
-    lower_letters_score = 0
-    upper_letters_score = 0
-    symbols_score = 0
-    midle_num_symb_score = 0
+def password_strength_calculate(password):
+    global_dict = globals()
     requirements = 0
-    requirements_score = 0
-
-    number_characters_score = lenght * 4
-        
-    if not numbers_only and digits_list:
-        numbers_score = len(''.join(digits_list)) * 4
-        requirements += 1
-    elif numbers_only:
-        requirements += 1
-    
-    if lower_letter:
-        lower_letters_score = (lenght - len(lower_letter)) * 2
-        requirements += 1
-        
-    if upper_letter:
-        upper_letters_score = (lenght - len(upper_letter)) * 2
-        requirements += 1
-        
-    if symbols:
-        symbols_score = len(symbols) * 6
-        requirements += 1
-        
-    if lenght >= 8:
-        requirements += 1
-        
-    if requirements == 5:
-        requirements_score = requirements * 2
-        
-    if midle_num_symb:
-        midle_num_symb_score = len(midle_num_symb) * 2
-    
-    return (number_characters_score ,numbers_score, lower_letters_score, 
-            upper_letters_score, symbols_score, midle_num_symb_score, requirements, requirements_score)
-
-
-def get_deductions_score(deductions_type, seq_letters, seq_numbers):
-    (numbers_only, letters_only, 
-     consecutive_upper_count, 
-     consecutive_lower_count, 
-     consecutive_num_count, 
-     sequense_letters, sequense_numbers)  = deductions_type
-    
-    letters_only_score = 0
-    numbers_only_score = 0
-    consecutive_upper_score = 0
-    consecutive_lower_score = 0
-    consecutive_number_score = 0
-    sequential_letters_score = 0
-    sequential_numbers_score = 0
-
-    if letters_only:
-        letters_only_score = -(len(letters_only))
-        
-    if numbers_only:
-        numbers_only_score = -(len(numbers_only))
-
-    if consecutive_upper_count:
-        consecutive_upper_score = -(consecutive_upper_count * 2)
-            
-    if consecutive_lower_count:
-        consecutive_lower_score = -(consecutive_lower_count * 2)
-        
-    if consecutive_num_count:
-        consecutive_number_score = -(consecutive_num_count * 2)
-       
-    if seq_letters:
-        sequential_letters_score = -(seq_letters * 3)
-    
-    if seq_numbers:
-        sequential_numbers_score = -(seq_numbers * 3)
-    
-    return (letters_only_score, numbers_only_score, consecutive_upper_score,
-            consecutive_lower_score, consecutive_number_score,
-            sequential_letters_score, sequential_numbers_score,
-            seq_letters, seq_numbers)
-
-
-def get_password_strength(additions_score, deductions_score):
-    total_score = []
-    total_score.extend(additions_score + deductions_score)
-    password_strength = round(sum(total_score)/10)
-    if password_strength > 10:
-        password_strength = 10   
-    elif password_strength < 1:
-        password_strength = 1
-    return password_strength
-
-
-def output_verbose_mode(add_type, deduct_type, add_score, deduct_score):
-    (lenght, digits_list, 
-     upper_letter, lower_letter,
-     symbols, midle_num_symb, numbers_only) = add_type
-    
-    (numbers_only, letters_only, 
-     consecutive_upper_count, 
-     consecutive_lower_count, 
-     consecutive_num_count, 
-     sequense_letters, sequense_numbers)  = deduct_type
-
-    (number_characters_score ,numbers_score, 
-     lower_letters_score, upper_letters_score, 
-     symbols_score, midle_num_symb_score, 
-     requirements, requirements_score) = add_score
-
-    (letters_only_score, numbers_only_score, 
-     consecutive_upper_score, consecutive_lower_score,
-     consecutive_number_score, sequential_letters_score, sequential_numbers_score, seq_letters, seq_numbers) = deduct_score
-
-    table_data = [['---------- Additions ----------', 'Rate','Count','Score'],
-                  ['Number of Characters', '+(n*4)', 
-                   lenght, '+' + str(number_characters_score)],
-                  ['Uppercase Letters', '+((len-n)*2)', 
-                   len(upper_letter), '+' + str(upper_letters_score)],
-                  ['Lowercase Letters', '+((len-n)*2)', 
-                   len(lower_letter), '+' + str(lower_letters_score)],
-                  ['Numbers', '+(n*4)', 
-                   len(''.join(digits_list)), '+' + str(numbers_score)],
-                  ['Symbols', '+(n*6)', 
-                   len(symbols), '+' + str(symbols_score)],
-                   ['Middle Numbers or Symbols', '+(n*2)', 
-                   len(midle_num_symb), '+' + str(midle_num_symb_score)],
-                   ['Requirements', '+(n*2)', 
-                   requirements, '+' + str(requirements_score)],
-                   ['---------- Deductions ----------','', '', ''], 
-                   ['Letters Only', '-n', 
-                   len(letters_only), str(letters_only_score)],
-                   ['Numbers Only', '-n', 
-                   len(numbers_only), str(numbers_only_score)],
-                   ['Consecutive Uppercase Letters', '-(n*2)', 
-                   consecutive_upper_count, str(consecutive_upper_score)],
-                   ['Consecutive Lowercase Letters', '-(n*2)', 
-                   consecutive_lower_count, str(consecutive_lower_score)],
-                   ['Consecutive Numbers', '-(n*2)', 
-                   consecutive_num_count, str(consecutive_number_score)],
-                   ['Sequential Letters (3+)', '-(n*3)', 
-                    seq_letters, str(sequential_letters_score)],
-                   ['Sequential Numbers (3+)', '-(n*3)', 
-                    seq_numbers, str(sequential_numbers_score)],
-                    ]      
-    
-    verbose_table = AsciiTable(table_data)
-    verbose_table.inner_row_border = True
-    verbose_table.justify_columns = {0: 'left', 1: 'center', 
-                                 2: 'center', 3: 'center'}
-    
-    return verbose_table.table
-
-
-def output_console(verbose, password):
-    seq_letters = check_sequential_alphas(password)
-    seq_numbers = check_sequential_numerics(password)
-    add_type = get_additions_type(password)
-    ded_type = get_deductions_type(password)
-    add_score = get_additions_score(add_type)
-    ded_score = get_deductions_score(ded_type, seq_letters, seq_numbers)
-    password_strength = get_password_strength(add_score, ded_score)
-
-    print('Total strength score: {}/10'.format(password_strength))
-    if args.verbose:
-        print(output_verbose_mode(add_type, ded_type, add_score, ded_score))
+    score_dict = {}
+    func_dict = { k : v for k,v in global_dict.items() if k.startswith('get_')}
+    for key, value in func_dict.items():
+        score_dict[key[4:]] = value(password)
+        if key[-3:] == 'req' and value(password)[0]:
+            requirements += 1
+    score_dict['requirements'] = (requirements, requirements * 2)
+    password_strength = sum(score[0] for score in score_dict.values())
+    return password_strength, score_dict
 
 
 def process_args():
     parser = argparse.ArgumentParser(description='Most starred repos find')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='output issues urls')
-    parser.add_argument('-hp', '--hide', action='store_true',
+    parser.add_argument('-H', '--hide', action='store_true',
                         help='Hide input password')
     return  parser.parse_args()
 
 
 if __name__ == '__main__':
+  
     args = process_args()
-    password = get_user_password(args.hide)
-    output_console(args.verbose, password)
+    password = input_user_password(args.hide)
+    
+
+
 
 
 
